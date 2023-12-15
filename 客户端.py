a5edffea5dfa2e6543d7813180344551e0ed01b5
Xe <3312902243@qq.com>
2023-12-15 11:32:20 +0800

    python的一问一答

---
+++ 客户端.py
@@ -0,0 +1,19 @@
+import socket
+
+socket_clent = socket.socket()
+#链接服务端
+socket_clent.connect(("localhost",8888))
+recv_data = socket_clent.recv(1024)
+print(f"服务端回复的消息是：{recv_data.decode('UTF-8')}")
+while True:
+    msg = input("输入要发送的消息:")
+    if msg == "exit":
+        break
+    socket_clent.send(msg.encode("UTF-8"))
+
+    recv_data = socket_clent.recv(1024)
+    if not recv_data:
+        break
+    print(f"服务端回复的消息是：{recv_data.decode('UTF-8')}")
+
+socket_clent.close()
---
+++ 服务端.py
@@ -0,0 +1,41 @@
+import socket
+
+socket_server = socket.socket()
+# 绑定ip和端口
+socket_server.bind(("localhost", 8888))
+# 监听端口
+socket_server.listen(1) # 允许链接的数量
+# 等待客户端链接
+result = socket_server.accept()
+# result是一个二元元组
+#conn = result[0]
+#address = result[1]
+conn,address = result
+#accept是阻塞方法，等待客户端连接卡在这
+print(f"接收到了客户端的链接，客户端信息：{address}")
+conn.send("你好客户端！".encode("UTF-8"))
+
+while True:
+    #接收客户端信息
+    data = conn.recv(1024).decode("UTF-8")
+    # recv接收的参数是缓冲区大小，1024即可
+    # recv返回是一个字节数字bytes对象，不是字符串，通过decode方法编码，将其转化成字符串对象
+    if not data:
+        break
+    print(f"客户端发来的信息是：{data}")
+    #发送回复消息
+    msg = input("请输入要发送客户端的消息：")
+    if msg == "exit":
+        break
+    conn.send(msg.encode("UTF-8"))
+
+#关闭连接
+conn.close()
+socket_server.close()
+
+
+
+
+
+
+
